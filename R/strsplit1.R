22978dd401d73c7fb323380579d91b80e446b942
gunishakaur <gunishakaur28@gmail.com>
2025-01-11 17:47:18 -0800

    packeges added

---
+++ R/strsplit1.R
@@ -0,0 +1,9 @@
+#' Split a string
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#' @return A character vector.
+#' @export
+strsplit1 <- function(x, split) {
+  strsplit(x, split = split)[[1]]
+}
